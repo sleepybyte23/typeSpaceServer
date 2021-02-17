cf2c9579ea0aff94a90da9d69e49bed459b1a8c0
Himanshu S <sleepybyte23@gmail.com>
2021-02-17 09:57:43 -0500

    initial

--- index.js
+++ index.js
@@ -1,42 +1,111 @@
-const app = require('express')()
-const http = require('http').createServer(app)
-const io = require('socket.io')(http)
-const cors = require('cors')
-const PORT = process.env.PORT || 5000
-const { addUser, getUser, deleteUser, getUsers } = require('./users')
+const app = require("express")();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http, {
+  pingInterval: 10000,
+  pingTimeout: 5000
+});
+//const io = require('socket.io')(3000);
+const redisAdapter = require('socket.io-redis');
+const cors = require("cors");
+const PORT = process.env.PORT || 5000;
+const {
+  addUser,
+  getUser,
+  deleteUser,
+  getUsers,
+  addRoom,
+  roomExist,
+  deleteRoom,
+} = require("./users");
 
+//io.adapter(redisAdapter({ host: 'localhost', port: 6379 }));
 
-app.use(cors())
+app.use(cors());
 
-io.on('connection', (socket) => {
-    socket.on('login', ({ name, room }, callback) => {
-        const { user, error } = addUser(socket.id, name, room)
-        if (error) return callback(error)
-        socket.join(user.room)
-        socket.in(room).emit('notification', { title: 'Someone\'s here', description: `${user.name} just entered the room` })
-        io.in(room).emit('users', getUsers(room))
-        callback()
-    })
+io.on("connection", (socket) => {
+  var logoffTimer;
 
-    socket.on('sendMessage', message => {
-        const user = getUser(socket.id)
-        io.in(user.room).emit('message', { user: user.name, text: message });
-    })
+  socket.on("login", ({ name, roomId, modCode }, callback) => {
+    console.log("login", name, roomId);
 
-    socket.on("disconnect", () => {
-        console.log("User disconnected");
-        const user = deleteUser(socket.id)
-        if (user) {
-            io.in(user.room).emit('notification', { title: 'Someone just left', description: `${user.name} just left the room` })
-            io.in(user.room).emit('users', getUsers(user.room))
-        }
-    })
-})
+    const { rm, err } = addRoom(roomId, modCode);
+    if (err) return callback(err);
 
-app.get('/', (req, res) => {
-    res.send("Server is up and running")
-})
+    const { user, error } = addUser(socket.id, name, roomId);
+    if (error) return callback(error);
+
+    socket.join(user.room);
+    socket
+      .in(roomId)
+      .emit("notification", {
+        title: "Someone's here",
+        description: `${user.name} just entered the room`,
+      });
+    io.in(roomId).emit("users", getUsers(roomId));
+    timerr(roomId);
+    callback();
+  });
+
+  socket.on("goto", ({ name, room, modCode }, callback) => {
+    console.log("login", name, room, modCode);
+
+    const { isModz, err } = roomExist(room, modCode);
+    if (err) return callback(err);
+    
+        const { user, error } = addUser(socket.id, name, room);
+        if (error) return callback(error);
+        socket.join(user.room);
+        //io.in(user.room).emit('message', { user: user.name, text: "tatttti" });
+        socket
+          .in(room)
+          .emit("notification", {
+            title: "Someone's here",
+            description: `${user.name} just entered the room`,
+          });
+        io.in(room).emit("users", getUsers(room));
+        //io.to(socket.id).emit('message', { user: user.name, text: "tatttti" });
+        timerr(user.room);
+        console.log("rmmmm", isModz);
+        callback(isModz);    
+  });
+
+  function timerr(roomId) {
+    clearTimeout(logoffTimer); 
+    let xx = roomId
+    logoffTimer = setTimeout(function(){
+      console.log('tooo', xx);
+      io.in(xx).emit("message", { user: "GOD", text: "logout" });
+      
+      deleteRoom(xx);
+      console.log(getUsers(xx));
+  }, 5000);
+  }
+
+  socket.on("sendMessage", (message) => {
+    const user = getUser(socket.id);
+    console.log(user);
+    io.in(user.room).emit("message", { user: user.name, text: message });
+    timerr(user.room);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+    const user = deleteUser(socket.id);
+
+    if (user) {
+      io.in(user.room).emit("notification", {
+        title: "Someone just left",
+        description: `${user.name} just left the room`,
+      });
+      io.in(user.room).emit("users", getUsers(user.room));
+    }
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Server is up and running");
+});
 
 http.listen(PORT, () => {
-    console.log(`Listening to ${PORT}`);
-})
+  console.log(`Listening to ${PORT}`);
+});
--- users.js
+++ users.js
@@ -1,28 +1,72 @@
-const users = []
+const users = [];
+const roomIds = {};
 
 const addUser = (id, name, room) => {
-    const existingUser = users.find(user => user.name.trim().toLowerCase() === name.trim().toLowerCase())
+  const existingUser = users.find(
+    (user) => user.name.trim().toLowerCase() === name.trim().toLowerCase()
+  );
 
-    if (existingUser) return { error: "Username has already been taken" }
-    if (!name && !room) return { error: "Username and room are required" }
-    if (!name) return { error: "Username is required" }
-    if (!room) return { error: "Room is required" }
+  if (existingUser) return { error: "Username has already been taken" };
+  if (!name && !room) return { error: "Username and room are required" };
+  if (!name) return { error: "Username is required" };
+  if (!room) return { error: "Room is required" };
 
-    const user = { id, name, room }
-    users.push(user)
-    return { user }
-}
+  const user = { id, name, room };
+  users.push(user);
+  return { user };
+};
 
-const getUser = id => {
-    let user = users.find(user => user.id == id)
-    return user
-}
+const getUser = (id) => {
+  let user = users.find((user) => user.id == id);
+  return user;
+};
+
+const addRoom = (roomId, modCode) => {
+  //console.log(roomIds, roomId, modCode, roomIds.indexOf(roomId));
+
+  if (roomId in roomIds) {
+    return { err: "Room already exist, on create" };
+  } else {
+    roomIds[roomId] = modCode;
+    console.log("roomIds on Create", roomIds);
+    return roomId;
+  }
+
+//   if (roomIds.indexOf(roomId) > -1) {
+//     return { err: "Room already exist, on create" };
+//   } else {
+//     const room = { roomId, modCode };
+//     roomIds.push(room);
+//     console.log(roomIds);    
+//     return room;
+//   }
+  //
+};
+
+const roomExist = (roomId, modCode) => {
+    console.log("ROOMIDS on join", roomIds, roomId, modCode);
+    if (roomId in roomIds) {
+        if(roomIds[roomId] === modCode) {
+            console.log("MOD__TRU");
+            return {isModz : true};
+        } else {
+            console.log("MOD__FALSE");
+            return {isModz : false};
+        }
+      } else {
+        return { err: "Room dont exist, on join" };
+      }  
+};
 
 const deleteUser = (id) => {
-    const index = users.findIndex((user) => user.id === id);
-    if (index !== -1) return users.splice(index, 1)[0];
+  const index = users.findIndex((user) => user.id === id);
+  if (index !== -1) return users.splice(index, 1)[0];
+};
+
+const deleteRoom = (roomId) => {
+  //users = users.filter((user) => user.room !== roomId);
+  return users;
 }
+const getUsers = (room) => users.filter((user) => user.room === room);
 
-const getUsers = (room) => users.filter(user => user.room === room)
-
-module.exports = { addUser, getUser, deleteUser, getUsers }
+module.exports = { addUser, getUser, deleteUser, getUsers, addRoom, roomExist, deleteRoom };
